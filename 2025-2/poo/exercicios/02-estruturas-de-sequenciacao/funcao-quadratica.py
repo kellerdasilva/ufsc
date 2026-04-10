f4954b02cac15305442e101b43fab1171e0c5cb3
Lucas Keller da Silva <lucaskeller27@gmail.com>
2026-04-10 19:46:11 -0300

    continua 02-01 de POO

--- 2025-2/poo/exercicios/02-estruturas-de-sequenciacao/funcao-quadratica.py
+++ 2025-2/poo/exercicios/02-estruturas-de-sequenciacao/funcao-quadratica.py
@@ -1,16 +1,57 @@
 import math
 
-# Obter os dados (a, b, c)
-a = int(input("Digite o valor de a: "))
-b = int(input("Digite o valor de b: "))
-c = int(input("Digite o valor de c: "))
+# Receber a entrada
 
-# Calcular delta
-delta = b ** 2 - 4*a*c
+a = int(input(""))
+b = int(input(""))
+c = int(input(""))
 
-# Calcular as raízes
-x1 = (-b + math.sqrt(delta)) / (2 * a)
-x2 = (-b - math.sqrt(delta)) / (2 * a)
+delta = b ** 2 - 4 * a * c
+x1 = (b * -1) + (math.sqrt(delta)) / 2 * a
+x2 = (b * -1) - (math.sqrt(delta)) / 2 * a
 
-# Imprimir o resultado
-print(f"As raízes reais são: {x1}, {x2}")
+print(x1, x2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import math
+
+# # Obter os dados (a, b, c)
+# a = int(input("Digite o valor de a: "))
+# b = int(input("Digite o valor de b: "))
+# c = int(input("Digite o valor de c: "))
+
+# # Calcular delta
+# delta = b ** 2 - 4*a*c
+
+# # Calcular as raízes
+# x1 = (-b + math.sqrt(delta)) / (2 * a)
+# x2 = (-b - math.sqrt(delta)) / (2 * a)
+
+# # Imprimir o resultado
+# print(f"As raízes reais são: {x1}, {x2}")
