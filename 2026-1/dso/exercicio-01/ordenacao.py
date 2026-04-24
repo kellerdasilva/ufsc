e8958026e25c8454df427cd3d3c48abd4c1c6e39
Lucas Keller da Silva <lucaskeller27@gmail.com>
2026-04-23 21:00:24 -0300

    inicia exercício 01 de DSO

---
+++ 2026-1/dso/exercicio-01/ordenacao.py
@@ -0,0 +1,18 @@
+class Ordenacao():
+
+    def ordena(self, array_para_ordenar: list):
+        
+        
+        """Realiza a ordenacao do conteudo do array recebido como parâmetro"""
+        ...
+        return ...
+
+    def to_string(self, array_ordenado: list):
+        """Converte o conteudo do array em String formatado
+           Exemplo: 
+           Para o conteudo do array: [1,2,3,4,5]
+           Retorna: "1,2,3,4,5"
+           @return String com o conteudo do array formatado
+     """
+        ...
+        return ...
